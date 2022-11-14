cff7810312440ac951b397de1b16969212ec2524
pafev <paulofvilarimda@gmail.com>
2022-11-14 15:38:08 -0300

    add files from exercise

---
+++ Q2JS.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        nome: "Aluno1",
+        idade: 18,
+        mediaFinal: 7,
+    },
+    {
+        nome: "Aluno2",
+        idade: 17,
+        mediaFinal: 10,
+    },
+    {
+        nome: "Aluno3",
+        idade: 16,
+        mediaFinal: 3,
+    },
+    {
+        nome: "Aluno4",
+        idade: 19,
+        mediaFinal: 8,
+    },
+    {
+        nome: "Aluno5",
+        idade: 21,
+        mediaFinal: 5,
+    },
+    {
+        nome: "Aluno6",
+        idade: 35,
+        mediaFinal: 6,
+    }    
+]
