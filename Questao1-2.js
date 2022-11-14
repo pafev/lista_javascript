85e086c1fd6bddc04d9c1eeba76372ca4bc883c0
pafev <paulofvilarimda@gmail.com>
2022-11-14 15:46:54 -0300

    question 1 item 2

---
+++ Questao1-2.js
@@ -0,0 +1,9 @@
+const generateUser = (nome, idade, email) => {
+    return {
+        nome: nome,
+        idade: idade,
+        email: email
+    }
+}
+
+// console.log(generateUser('Paulo', '19', 'example@email'))
